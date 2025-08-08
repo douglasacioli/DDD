0e399555b8c6a0830c059b1d97c8e92ff7314992
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-08 11:49:38 -0300

    exclusão de classe desnecessária na service

--- Api.Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Api.Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5640f675f0cd67a68e5a99ac071fc0105fbfaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
