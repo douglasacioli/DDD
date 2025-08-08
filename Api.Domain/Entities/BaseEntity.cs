bb1b51086d93e8c4de6562da13d6969551c2b94b
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-08 12:32:05 -0300

    propriedades de usuário

--- Api.Domain/Entities/BaseEntity.cs
+++ Api.Domain/Entities/BaseEntity.cs
@@ -6,15 +6,12 @@ namespace Api.Domain.Entities
     {
         [Key]
         public Guid Id { get; set; }
-
         private DateTime? _createdAt;
         public DateTime? CreatedAt
         {
             get => _createdAt;
             set => _createdAt = value ?? DateTime.UtcNow;
         }
-
         private DateTime? UpdatedAt { get; set; }
-      
     }
 }
