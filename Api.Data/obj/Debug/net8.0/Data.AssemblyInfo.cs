9ff93dacafb54fd4466cbbdb3b1dc2d73e4d3876
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-15 23:12:52 -0300

    criação da classe ContextFactory com as configurações de acesso ao meu banco de dados

--- Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9fcae2b5bb2d65788d5ce9e7a03661c72a7b35a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf180f366408251096c309085f1f585c88e259d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
