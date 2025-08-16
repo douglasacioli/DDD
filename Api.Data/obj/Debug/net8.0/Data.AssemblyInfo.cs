9726d9d6fbd8f2f52df6c88bea9e625f6403353b
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-15 23:23:41 -0300

    configuração do userMap

--- Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf180f366408251096c309085f1f585c88e259d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff93dacafb54fd4466cbbdb3b1dc2d73e4d3876")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
