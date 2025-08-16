ba3186f33988595fe8a2d4e5ddcd59437d266142
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-16 00:04:55 -0300

    interface de repository

--- Api.Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Api.Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9726d9d6fbd8f2f52df6c88bea9e625f6403353b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f259a1b0da3c813b80268708150b26e9cacd9d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
