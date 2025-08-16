df4fa48157ce2355f29cb2a3bd7eddf807856e1c
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-16 00:04:55 -0300

    interface de repository

---
+++ Api.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+using Api.Domain.Entities;
+namespace Api.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> InsertAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task<bool> DeleteAsync(Guid id);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
