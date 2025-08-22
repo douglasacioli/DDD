97ee3c2a1f06ec3f8f8b242efbfaf30722b68259
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-21 23:23:59 -0300

    fazendo uso da IUserInterface

---
+++ Api.Service/Services/UserService.cs
@@ -0,0 +1,41 @@
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.User;
+
+namespace Api.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<UserEntity> _repository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<UserEntity> Post(UserEntity user)
+        {
+            return await _repository.InsertAsync(user);
+        }
+
+        public async Task<UserEntity> Put(UserEntity user)
+        {
+            return await _repository.UpdateAsync(user);
+        }
+        }
+}
