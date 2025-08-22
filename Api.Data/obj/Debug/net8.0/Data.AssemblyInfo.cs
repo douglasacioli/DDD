3d4f74e976e4bf7282f899740a6d97266f9b6323
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-21 23:14:26 -0300

    referenciando api.data, api.domain e api.crosscutting na api.service

--- Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Api.Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a2321bdf7b0880333615c2d23d633419dd1eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7807f3fa79d2d4fea0a3f0bf5c9dae312562bbfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
