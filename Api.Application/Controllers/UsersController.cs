bf63cd18a3c83fd7d66d2a9e00e488c494886d16
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-21 23:57:26 -0300

    referenciando domain,data e service a api.crosscutting

--- Api.Application/Controllers/UsersController.cs
+++ Api.Application/Controllers/UsersController.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
 
     }
 }
